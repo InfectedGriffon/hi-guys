0cd7766a9c40a0eebc791d8f5257141079ad7326
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-04-22 22:54:25 -0400

    fix spacing and stuff

--- javascript/makeok.js
+++ javascript/makeok.js
@@ -3,15 +3,17 @@ import fs from 'fs';
 let content = "let hey = {"
 let closingBrackets = "}"
 let callers = "."
-for (const c of 'abcdefghijklmnopqrstuvwxyz') {
-    content += c+":{"
+
+for (const letter of 'abcdefghijklmnopqrstuvwxyz') {
+    content += letter+":{"
     closingBrackets += "}"
-    callers += c+"."
+    callers += letter+"."
 };
+
 content += 'final:"woah"'
-content += closingBrackets
-
-content += "\n\nconsole.log(hey" + callers + "final)"
-
+        + closingBrackets
+        + "\n\nconsole.log(hey"
+        + callers
+        + "final)"
 
 fs.writeFileSync('javascript/ok.js', content, e=>debug.log(e))
