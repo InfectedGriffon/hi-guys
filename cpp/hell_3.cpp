849b2a7ec6e8bcca79f86eabb329c12c90644c2c
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-05-19 18:22:22 -0400

    hellish c++ code

---
+++ cpp/hell_3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    auto _ = [](){ 
+        puts((const char*)(const int[]) {
+            0x6c6c6548,
+            0x77202c6f,
+            0x646c726f,
+            0x21
+        });
+    };
+    
+    _();
+}
---
+++ cpp/hell_4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    puts((const char*)(const int[]) {
+        0x6c6c6548,
+        0x77202c6f,
+        0x646c726f,
+        0x21
+    });
+}
