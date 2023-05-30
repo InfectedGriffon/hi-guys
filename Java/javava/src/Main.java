412c6404b4c19a7e7fbe871664864de88fe37c48
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-05-29 22:01:49 -0400

    what the hell is a predicate

--- Java/javava/src/Main.java
+++ Java/javava/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.function.Predicate;
+
 public class Main {
+    public static Predicate<Integer> is_even = n -> n % 2 == 0;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.printf("is four even? %b", is_even.test(4));
     }
 }
